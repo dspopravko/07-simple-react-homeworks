0bfe46231babdfd81a03b6704a38a0e599a20b3b
Demid Popravko <demid1498@gmail.com>
2022-08-18 13:21:49 +0300

    homework - 01

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,10 +1,36 @@
 import React from 'react'
+import s from './Message.module.css'
 
-function Message() {
+type MessagePropsType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props: MessagePropsType) {
     return (
-        <div>
+        <>
+            <div className={s.message}>
+                <div className={s.avatar_container}>
+                    <img alt="avatar" src={props.avatar}/>
+                </div>
+                <div className={s.message_container}>
+                    <div className={s.name}>
+                        {props.name}
+                    </div>
+                    <div className={s.body_container}>
+                        <div className={s.text_wrapper}>
+                            <p className={s.text}>{props.message}</p>
+                        </div>
+                        <div className={s.time_container}>
+                            <div className={s.time}>{props.time}</div>
+                        </div>
+                    </div>
+                </div>
 
-        </div>
+            </div>
+        </>
     )
 }
 
