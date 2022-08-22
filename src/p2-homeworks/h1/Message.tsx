7d6457026e4581f6e1573cecd3015af4325896b4
Demid Popravko <demid1498@gmail.com>
2022-08-22 11:19:25 +0300

    homework - 01 cutout resize fix

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -15,7 +15,9 @@ function Message(props: MessagePropsType) {
                 <div className={s.avatar_container}>
                     <img alt="avatar" src={props.avatar}/>
                 </div>
-                <svg viewBox="0 0 100 100"><path d="M100,0v100H18.2c0.3-1.2,3.6-1.8,13.6-5.2c14.8-5,28.1-13.3,38.9-24.1C88.8,52.6,100,27.6,100,0z"/></svg>
+                <div className={s.svgWrapper}>
+                    <svg viewBox="0 0 100 100"><path d="M100,0v100H18.2c0.3-1.2,3.6-1.8,13.6-5.2c14.8-5,28.1-13.3,38.9-24.1C88.8,52.6,100,27.6,100,0z"/></svg>
+                </div>
                 <div className={s.message_container}>
                     <div className={s.name}>
                         {props.name}
